ec5b111ba7963bff0c7211d6aac5e7d85f41bd3f
Fuat <fuatozerr23@gmail.com>
2019-03-03 02:36:49 +0300

    Yorum silme ve güncelleme işlemi ajax ile yapıldı.

--- MyEvernoteSolution/MyEvernote.WebApp/Controllers/CommentController.cs
+++ MyEvernoteSolution/MyEvernote.WebApp/Controllers/CommentController.cs
@@ -13,6 +13,7 @@ namespace MyEvernote.WebApp.Controllers
     public class CommentController : Controller
     {
         private NoteManager noteManager = new NoteManager();
+        private CommentManager commentManager = new CommentManager();
         // GET: Comment
         public ActionResult ShowNoteComments(int? id)
         {
@@ -22,6 +23,60 @@ namespace MyEvernote.WebApp.Controllers
             }
             Note note = noteManager.ListQueryable().Include("Comments").FirstOrDefault(x => x.Id == id);
             return PartialView("_PartialComments",note.Comments);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int? id,string text)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Comment comment = commentManager.Find(x => x.Id == id);
+
+            if(comment==null)
+            {
+                return new HttpNotFoundResult();
+            }
+
+            comment.Text = text;
+            if(commentManager.Update(comment)>0)
+            {
+                return Json(new { result = true }, JsonRequestBehavior.AllowGet);
+            }
+            return Json(new { result = false }, JsonRequestBehavior.AllowGet);
+
+
+        }
+
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Comment comment = commentManager.Find(x => x.Id == id);
+
+            if (comment == null)
+            {
+                return new HttpNotFoundResult();
+            }
+
+
+            if(commentManager.Delete(comment)> 0)
+            {
+                return Json(new { result = true }, JsonRequestBehavior.AllowGet);
+
+            }
+
+            else
+            {
+                return Json(new { result = false }, JsonRequestBehavior.AllowGet);
+
+            }
         }
     }
 }
